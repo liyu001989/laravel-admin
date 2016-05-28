1bf7626181106688aa19efcf37bb04f56cec5f84
Yu Li <liyu001989@gmail.com>
2016-05-29 00:29:33 +0800

    add bower gulp

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # laravel-admin-demo
 
-工作中使用adminLTE写的后台，挺好用的，整理一下，写个demo
+工作中使用 adminLTE 写的后台，挺好用的，整理一下，写个demo
+
+没写完，一点一点弄
+
+## usage
+
+### normal
+- install composer npm bower gulp
+- composer install
+- npm install
+- bower install
+- cp .env.example .env
+- php artisan key:generate
+- configure your .env
+- php artisan migrate
+- php artisan db:seed
+- gulp
+
+### easy
+- envoy run update
+
--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,28 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+
+    // css
+    // mix.copy('resources/assets/bower/AdminLTE/dist/css/AdminLTE.min.css', 'public/css/AdminLTE.min.css');
+    // mix.copy('resources/assets/bower/AdminLTE/dist/css/skins/skin-red-light.min.css', 'public/css/skin-red-light.min.css');
+    // mix.copy('resources/assets/bower/bootstrap/dist/css/bootstrap.min.css', 'public/css/skin-red-light.min.css');
+
+    // js
+    mix.copy('resources/assets/bower/AdminLTE/dist/js/app.min.js', 'public/js/AdminLTE.min.js');
+    mix.copy('resources/assets/bower/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+    mix.copy('resources/assets/bower/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+
+    // minify
+    mix.scripts(['../bower/bootbox.js/bootbox.js'], 'public/js/bootbox.min.js');
+
+    // fonts
+    mix.copy('resources/assets/bower/font-awesome/fonts/', 'public/fonts/');
+
+    mix.styles([
+        '../bower/AdminLTE/dist/css/AdminLTE.min.css',
+        '../bower/AdminLTE/dist/css/skins/skin-red-light.min.css',
+        '../bower/bootstrap/dist/css/bootstrap.min.css',
+        '../bower/font-awesome/css/font-awesome.min.css',
+    ]);
+
 });
