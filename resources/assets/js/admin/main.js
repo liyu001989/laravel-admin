5f032403c158cfd45b062555d4a96339517802a4
Yu Li <liyu001989@gmail.com>
2016-06-12 23:58:05 +0800

    add csrf

--- resources/assets/js/admin/main.js
+++ resources/assets/js/admin/main.js
@@ -1,18 +1,26 @@
 require.config({
-    baseUrl: "/js/",
-    paths: {
-        jquery: "jquery.min",
-        bootstrap: 'bootstrap.min',
-        bootbox: 'bootbox.min',
-        adminLTE: 'AdminLTE.min',
-        slimscroll: 'jquery.slimscroll.min',
-    },
-    shim: {
-        'adminLTE': ['jquery', 'bootstrap', 'slimscroll'],
-        'bootbox': ['jquery', 'bootstrap'],
-        'bootstrap': ['jquery'],
-        'slimscroll': ['jquery']
-    }
-});
+  baseUrl: '/js/',
+  paths: {
+    jquery: 'jquery.min',
+    bootstrap: 'bootstrap.min',
+    bootbox: 'bootbox.min',
+    adminLTE: 'AdminLTE.min',
+    slimscroll: 'jquery.slimscroll.min'
+  },
+  shim: {
+    'adminLTE': ['jquery', 'bootstrap', 'slimscroll'],
+    'bootbox': ['jquery', 'bootstrap'],
+    'bootstrap': ['jquery'],
+    'slimscroll': ['jquery']
+  }
+})
 
-require(['adminLTE']);
+require(['adminLTE'])
+
+require(['jquery'], function ($) {
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  })
+})
--- resources/views/admin/common/layout.blade.php
+++ resources/views/admin/common/layout.blade.php
@@ -6,6 +6,8 @@
     <title>AdminLTE 2 | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap 3.3.6 -->
     <link href="{{asset('css/all.css')}}" rel="stylesheet" type="text/css" />
     <!-- app css  -->
